17ed1ef7e4e39eabb1401775eb56add5f3833e30
schaare <tino.schaare@hpe.com>
2024-11-09 12:00:32 +0100

    airflow

--- airflow/mtg_airflow.py
+++ airflow/mtg_airflow.py
@@ -8,6 +8,14 @@ args = {
     'owner': 'airflow'
 }
 
+dag = DAG('MTG_Crawler', 
+          default_args=args, 
+          description='Crawling and providing MTG card information',
+          schedule_interval='*/5 * * * *',
+          start_date=datetime(2019, 10, 16), 
+          catchup=False, 
+          max_active_runs=1)
+
 # Functions ---------------------------------------------------------------------
 
 def check_connection():
@@ -16,16 +24,8 @@ def check_connection():
 # Operators ---------------------------------------------------------------------
 check_connection_op = PythonOperator(
     task_id="check_connection",
-    python_callable=check_connection
+    python_callable=check_connection,
+    dag=dag
 )
-
-
-dag = DAG('MTG_Crawler', 
-          default_args=args, 
-          description='Crawling and providing MTG card information',
-          schedule_interval='*/5 * * * *',
-          start_date=datetime(2019, 10, 16), 
-          catchup=False, 
-          max_active_runs=1)
 
 check_connection_op
