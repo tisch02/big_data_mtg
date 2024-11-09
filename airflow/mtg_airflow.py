7d9acfab44ddb046095217ebc9b196f7a73b082f
schaare <tino.schaare@hpe.com>
2024-11-09 11:17:20 +0100

    airflow

---
+++ airflow/mtg_airflow.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from airflow import DAG
+
+args = {
+    'owner': 'airflow'
+}
+
+dag = DAG('MTG Crawler', 
+          default_args=args, 
+          description='Crawling and providing MTG card information',
+          schedule_interval='*/5 * * * *',
+          start_date=datetime(2019, 10, 16), 
+          catchup=False, 
+          max_active_runs=1)
